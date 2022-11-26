088ffa6126d10b669e7e82bf54c31e5e07b0c59d
whatthefoobar <irina.mntn@gmail.com>
2022-11-26 10:09:38 +0100

    updated about me

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './About.css';
-import pic from '../../img/pic.jpg';
+import React from "react";
+import "./About.css";
+import pic from "../../img/pic.jpg";
 
 const About = () => {
   return (
@@ -16,13 +16,15 @@ const About = () => {
         <div className="content">
           {/* <h3 className="title">I am a developer</h3> */}
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
-            non. Ea magni soluta fugiat cum amet quibusdam facilis itaque
-            dolorem dolorum architecto reiciendis ut minima, atque dolore, optio
-            inventore enim totam quam quasi ab ex. Distinctio illum vitae quo,
-            culpa praesentium, corrupti ipsam obcaecati reiciendis, ipsum beatae
-            officiis animi! Voluptas ratione eaque cum autem, sed blanditiis
-            earum nam impedit deleniti iure neque minus rerum natus maxime.
+            “A jack of all trades is a master of none, but oftentimes better
+            than a master of one.” I like this saying because it best describes
+            my approach to learning. Often a cliché, I do consider myself a
+            life-time learner, after studying economics and psychology and
+            working in customer oriented jobs I decided to finally bite the
+            bullet and take on potentially the biggest challenge I had so far :
+            get into frontend development. Coding challenges me to find
+            solutions to unique problems all the while keeping me humble and
+            focused on a goal that does not have a clear timeline.
           </p>
           <button className="btn">Learn more</button>
         </div>
