5997def7a9a69dc8fd663a8643628c124380f051
whatthefoobar <irina.mntn@gmail.com>
2023-06-05 16:38:47 +0200

    updated router to latest syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
-import Skills from "./pages/Skills/Skills";
-import Work from "./pages/Work/Work";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+      <Outlet />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Home from "./pages/Home/Home.tsx";
+import About from "./pages/About/About.tsx";
+import Contact from "./pages/Contact/Contact.tsx";
+import Skills from "./pages/Skills/Skills.tsx";
+import Work from "./pages/Work/Work.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/work" element={<Work />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
