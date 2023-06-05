a8a5efc98872ba9b1d69c908fd9c87e697421fb2
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2023-06-05 10:09:41 +0200

    Update Work.js

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -112,7 +112,7 @@ const Work = () => {
       description:
         "A form that takes in your birthday and returns a serial killer with the same zodiac sign as you. For this i used a simple Node webscraper to get a killer for each zodiac sign. Queries are stored with localStorage.",
       code: "https://github.com/whatthefoobar/zodiac-project",
-      preview: "#",
+      preview: "https://zodiac-killer.onrender.com/",
     },
   ];
 
