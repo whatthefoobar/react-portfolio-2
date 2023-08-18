b684f575aed2915654df6221758db5175b3f44af
whatthefoobar <irina.mntn@gmail.com>
2023-08-18 09:34:28 +0200

    some changes

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -4,13 +4,14 @@ import p1 from "../../assets/img/p6.png";
 import p2 from "../../assets/img/p3.jpg";
 import p3 from "../../assets/img/p5.png";
 import p4 from "../../assets/img/p7.png";
-import p5 from "../../assets/img/p9.png";
-import p6 from "../../assets/img/p10.png";
+// import p5 from "../../assets/img/p9.png";
+// import p6 from "../../assets/img/p10.png";
 import p7 from "../../assets/img/p4.jpg";
 import p8 from "../../assets/img/p2.jpg";
 import p9 from "../../assets/img/p11.png";
 import p10 from "../../assets/img/p12.png";
 import p11 from "../../assets/img/p13.png";
+import p12 from "../../assets/img/p14.png";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import { IProject } from "../../types/types";
 
@@ -63,22 +64,22 @@ const projects: IProject[] = [
     code: "https://github.com/whatthefoobar/50daysofcode-project-gallery",
     preview: "https://50dayofcode-project-gallery.netlify.app/",
   },
-  {
-    name: "LinkedIn clone",
-    image: [p5],
-    description:
-      " A simplified Linkedin clone using React, Redux, MUI icons and Firebase for authentication and database.",
-    code: "https://github.com/whatthefoobar/react-linkedin-clone",
-    preview: "https://linkedin-clone-8fbcd.web.app/",
-  },
-  {
-    name: "Gmail Clone",
-    image: [p6],
-    description:
-      " A simplified Google clone using React, Redux, MUI icons and Firebase for authentication and database.",
-    code: "https://github.com/whatthefoobar/react-gmail-clone",
-    preview: "https://clone-174af.web.app",
-  },
+  // {
+  //   name: "LinkedIn clone",
+  //   image: [p5],
+  //   description:
+  //     " A simplified Linkedin clone using React, Redux, MUI icons and Firebase for authentication and database.",
+  //   code: "https://github.com/whatthefoobar/react-linkedin-clone",
+  //   preview: "https://linkedin-clone-8fbcd.web.app/",
+  // },
+  // {
+  //   name: "Gmail Clone",
+  //   image: [p6],
+  //   description:
+  //     " A simplified Google clone using React, Redux, MUI icons and Firebase for authentication and database.",
+  //   code: "https://github.com/whatthefoobar/react-gmail-clone",
+  //   preview: "https://clone-174af.web.app",
+  // },
   {
     name: "RPG Caves game",
     image: [p7],
@@ -111,6 +112,14 @@ const projects: IProject[] = [
       "A form that takes in your birthday and returns a serial killer with the same zodiac sign as you. For this i used a simple Node webscraper to get a killer for each zodiac sign. Queries are stored with localStorage.",
     code: "https://github.com/whatthefoobar/zodiac-project",
     preview: "https://zodiac-killer.onrender.com/",
+  },
+  {
+    name: "Simple React flashcard app",
+    image: [p12],
+    description:
+      "A simple flashcard app using React with Typescript for learning some Mandarin phrases.",
+    code: "https://github.com/whatthefoobar/react-flashcards",
+    preview: "https://react-flashcards-app.netlify.app/",
   },
 ];
 
