b78de693c77d4233498676cbba22de0e7696e73c
whatthefoobar <irina.mntn@gmail.com>
2024-03-26 08:50:22 +0100

    changed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Navbar2 from "./components/Navbar2/Navbar2";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar2 />
       <Outlet />
     </>
   );
---
+++ src/components/Navbar2/Navbar2.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar2.css";
+import { Link } from "react-router-dom";
+
+const Navbar2 = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <Link to="/" className="navbar-item">
+          Home
+        </Link>
+        <Link to="/about" className="navbar-item">
+          About
+        </Link>
+        <Link to="/skills" className="navbar-item">
+          Skills
+        </Link>
+        <Link to="/work" className="navbar-item">
+          Work
+        </Link>
+        <Link to="/contact" className="navbar-item">
+          Contact
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar2;
