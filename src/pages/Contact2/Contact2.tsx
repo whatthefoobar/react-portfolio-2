c3ed87cdcd115419115089d9a5740c11b9838641
whatthefoobar <irina.mntn@gmail.com>
2024-06-14 11:41:06 +0200

    cleaner layout clean

--- src/pages/Contact2/Contact2.tsx
+++ src/pages/Contact2/Contact2.tsx
@@ -8,32 +8,31 @@ import { BsLinkedin } from "react-icons/bs";
 
 const Contact2: React.FC = () => {
   return (
-    <div className="contactSection">
+    <div className="contact">
+      <h1 className="heading">
+        <span>Contact</span> me
+      </h1>
       <div className="container">
-        <div className="sectionTitle">
-          <h2>Contact</h2>
-          <p>Get in touch</p>
-        </div>
-        <div className="contactSection__wrapper">
+        <div className="contact-wrapper">
           <div className="right">
             <ContactForm />
           </div>
           <div className="left">
-            <div className="contact__box">
+            <div className="contact-box">
               <a href="https://github.com/whatthefoobar">
                 <FaGithub className="contact-social" />
                 <h3>Github</h3>
               </a>
             </div>
 
-            <div className="contact__box">
+            <div className="contact-box">
               <a href="mailto:irina.mntn@gmail.com">
                 <FaMailBulk className="contact-social" />
                 <h3>Email</h3>
               </a>
             </div>
 
-            <div className="contact__box">
+            <div className="contact-box">
               <a href="https://www.linkedin.com/in/irina-munteanu-b%C3%A4ck-523a41b4/">
                 <BsLinkedin className="contact-social" />
                 <h3>LinkedIn</h3>
