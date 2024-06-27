d665d55cefdc678647b19a9acae56e58913c2f87
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2024-06-27 10:23:49 +0200

    Update Work.tsx

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,6 +1,6 @@
 import "./Work.css";
 import p1 from "../../assets/img/p1.png";
-import p2 from "../../assets/img/p2.png";
+//import p2 from "../../assets/img/p2.png";
 import p3 from "../../assets/img/p3.png";
 import p4 from "../../assets/img/p4.png";
 import p5 from "../../assets/img/p5.png";
