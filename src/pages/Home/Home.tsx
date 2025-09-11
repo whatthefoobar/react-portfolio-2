6d2afdef24bc28e03a5c56fff8bfef2e21e5c2e9
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2025-09-11 15:40:00 +0200

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <section id="home" className="home container">
       <h3>HI THERE !</h3>
       <h1>
-        I'M <span>Irina Munteanu Bäck</span>
+        I'M <span>Irina Bäck</span>
       </h1>
       <p>
         Frontend Developer - React | TypeScript | Node.js | Redux | MongoDB | Express.js
