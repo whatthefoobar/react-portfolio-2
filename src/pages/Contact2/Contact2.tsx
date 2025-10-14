09c0deace6f6cf71c150f6e40bae5e1c5c19e656
Irina Bäck <69626975+whatthefoobar@users.noreply.github.com>
2025-10-14 13:53:39 +0200

    Update Contact2.tsx

--- src/pages/Contact2/Contact2.tsx
+++ src/pages/Contact2/Contact2.tsx
@@ -33,7 +33,7 @@ const Contact2: React.FC = () => {
             </div>
 
             <div className="contact__box">
-              <a href="https://www.linkedin.com/in/irina-munteanu-b%C3%A4ck-523a41b4/">
+              <a href="https://www.linkedin.com/in/irina-back/">
                 <BsLinkedin className="contact-social" />
                 <h3>LinkedIn</h3>
               </a>
